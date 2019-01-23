d07c07b7719d7aacb2ef11c51c6981d853a9f166
danielszakacs <daniel.sokach@gmail.com>
2019-01-23 15:42:43 +0100

    frontend css updated. but still need to update

--- src/main/angular/portalfrontend/src/app/app.module.ts
+++ src/main/angular/portalfrontend/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import {HttpClientModule} from "@angular/common/http";
 
-
 @NgModule({
   declarations: [
     AppComponent,
--- src/main/angular/portalfrontend/src/app/main-page/main-page.component.ts
+++ src/main/angular/portalfrontend/src/app/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from "../../environments/environment.prod";
 import {HttpClient} from "@angular/common/http";
+import {APP_BOOTSTRAP_LISTENER} from "@angular/core";
 
 @Component({
   selector: 'app-main-page',
