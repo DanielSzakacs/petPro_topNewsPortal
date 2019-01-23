60bb8ec7668447d2be8c69ac3604a76512739568
danielszakacs <daniel.sokach@gmail.com>
2019-01-23 13:24:09 +0100

    frontend created.

---
+++ src/main/angular/portalfrontend/src/app/main-page/main-page.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from "../../environments/environment.prod";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+
+  mainPageNews = environment.mainNewsURL;
+  listOfNews : Object;
+  listOfTopNews : Object;
+  sportLine: Object;
+  languageText = environment.engText;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getAllNews();
+    this.getSportLine();
+  }
+
+  getAllNews(){
+    this.getAllTopNewsBySource('cnn');
+    this.http.get(this.mainPageNews).subscribe(
+      res => {
+        this.listOfNews = res;
+        console.log(res)
+      },
+      error1 => {
+        console.log(error1)
+      }
+    );
+  }
+
+  selectNews(source) {
+    let news = source.target.value;
+    this.getAllTopNewsBySource(news);
+    console.log(news);
+    this.http.get(this.mainPageNews + '/' + news).subscribe(
+      res => {
+        this.listOfNews = res;
+        console.log(res)
+      },
+      error1 => {
+        console.log(error1)
+      }
+    );
+  }
+
+  getAllTopNewsBySource(source) {
+    this.http.get(this.mainPageNews + '/' + source + '?type=top').subscribe(
+      res => {
+        this.listOfTopNews = res;
+        console.log("Get all the Top news was Good.")
+        console.log(this.listOfTopNews)
+      },
+      error1 => {
+        console.log(error1)
+      }
+    )
+  }
+
+  getSportLine() {
+    this.http.get(this.mainPageNews + '/bbc-sport' ).subscribe(
+      res => {
+        this.sportLine = res;
+      },
+      error1 => {
+        console.log(error1)
+      }
+    )
+  }
+
+  changeLanguage() {
+    if(this.languageText == environment.engText){
+      this.languageText = environment.rusText;
+    }else{
+      this.languageText = environment.engText;
+    }
+  }
+
+}
--- src/main/angular/portalfrontend/src/environments/environment.prod.ts
+++ src/main/angular/portalfrontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  mainNewsURL: 'http://localhost:8081/topnews',
+  rusText: {'World':'Новости', 'Business':'Бизнес',
+    'Sport':'Спорт', 'Crypto':'Крипто',
+    'In_focus':'Фокус', 'Top_news':'Новости',
+    'Logo': 'ЛОГО', 'Around_the_world': 'Мир', 'All_news': 'Все'},
+  engText: {'World':'World', 'Business':'Business',
+    'Sport':'Sport', 'Crypto':'Crypto',
+    'In_focus':'In focus', 'Top_news':'Top stories',
+    'Logo': 'LOGO', 'Around_the_world': 'Around the world',
+    'All_news': 'All news'}
 };
