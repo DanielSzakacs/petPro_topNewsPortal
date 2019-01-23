559cf9101f174ff4a1c876e2200f3dab265a272a
danielszakacs <daniel.sokach@gmail.com>
2019-01-23 11:42:09 +0100

    main folders created.

---
+++ src/main/java/com/codecool/petproject/newspostal/NewspostalApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.petproject.newspostal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspostalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspostalApplication.class, args);
+    }
+
+}
+
