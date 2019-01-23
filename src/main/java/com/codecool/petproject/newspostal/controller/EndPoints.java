6591f5b24b9349791c45e753ec8d8def6fc6264f
danielszakacs <daniel.sokach@gmail.com>
2019-01-23 13:10:01 +0100

    backend created.

---
+++ src/main/java/com/codecool/petproject/newspostal/controller/EndPoints.java
@@ -0,0 +1,39 @@
+package com.codecool.petproject.newspostal.controller;
+
+import com.codecool.petproject.newspostal.service.NewsFactory;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+public class EndPoints {
+
+    private NewsFactory newsFactory = new NewsFactory();
+    //al-jazeera-english  cnn  fox-news  abc-news  google-news-ru  independent  rt
+    @GetMapping("/topnews/{source}")
+    public String getNewsBySource(@PathVariable String source, @RequestParam(name = "type", required = false) String type)  {
+        type = type == null ? "" : type;
+        if(source.equals("rt")){ //This part is necessary because other ways we get all the rt news which is on spanish.
+            type = "top";
+        }
+        try {
+            return String.valueOf(newsFactory.getNews(source, type));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "The is no data";
+        }
+    }
+
+    @GetMapping("/topnews")
+    public String getAllNews() {
+        try {
+            return String.valueOf(newsFactory.getNews("cnn", "")); // TODO delete the string. Only for test
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "The is no data";
+        }
+    }
+
+    private void testingMethod() {
+        System.out.println("ok");
+    }
+}
---
+++ src/main/java/com/codecool/petproject/newspostal/service/NewsFactory.java
@@ -0,0 +1,21 @@
+package com.codecool.petproject.newspostal.service;
+import org.json.JSONObject;
+
+public class NewsFactory {
+
+    public Object getNews(String channelName, String newsType) throws Exception {
+        return buildNews(channelName, newsType);
+    }
+
+    private Object buildNews(String channelName, String newsType) throws Exception{
+        Object newsObject = getNewsApiRead(channelName, newsType).get("articles");
+        return newsObject;
+    }
+
+    private JSONObject getNewsApiRead(String channelName, String newsType) throws Exception {
+        String type;
+        type = newsType.equals("top") ? "top-headlines" : "everything";
+        String url = "https://newsapi.org/v2/"+ type +"?sources=" + channelName + "&apiKey=707805dad8ed44f1b6c24adb59e2739c";
+        return UrlReader.getJsonObjectFromUrl(url);
+    }
+}
