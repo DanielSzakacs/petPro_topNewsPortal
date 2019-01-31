db18d4812ed54926e24e14d2c10d4ac3c3626940
danielszakacs <daniel.sokach@gmail.com>
2019-01-31 20:16:14 +0100

    frontend features / top news card created. Ready for deploy

--- src/main/client/src/environments/environment.prod.ts
+++ src/main/client/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   mainNewsURL: '/topnews',
   // mainNewsURL: 'http://localhost:8080/topnews',
-  rusText: {'World':'Новости', 'Business':'Бизнес',
+  rusText: {'World':'Мир', 'Business':'Бизнес',
     'Sport':'Спорт', 'Crypto':'Крипто',
     'In_focus':'Фокус', 'Top_news':'Новости',
     'Logo': 'ЛОГО', 'Around_the_world': 'Мир', 'All_news': 'Все'},
