bd7b2ac721a7083cccd08448fb1d2462b3193e33
danielszakacs <daniel.sokach@gmail.com>
2019-02-15 16:03:00 +0100

    newsFactory covered with test

--- src/test/java/com/codecool/petproject/newspostal/NewsFactoryTest.java
+++ src/test/java/com/codecool/petproject/newspostal/NewsFactoryTest.java
@@ -1,15 +1,42 @@
 package com.codecool.petproject.newspostal;
 
+import com.codecool.petproject.newspostal.service.NewsFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class NewsFactoryTest {
 
-    @Test()
-    public void newsFactory_getAllNews_returnEmptyString() {
+    NewsFactory newsFactory;
+
+    @Test
+    public void setNewsFactory_jsonBuilderMethod_returnEmptyString(){
+        newsFactory = new NewsFactory();
+        assertEquals("",newsFactory.jsonBuilder("", ""));
+    }
+
+    @Test
+    public void setNewsFactory_getNewsByType_willReturn10NewsIfTypeIsTop() throws JSONException { // TODO Balazs nézze meg
+        newsFactory = new NewsFactory();
+        JSONObject jsonNews = new JSONObject(newsFactory.getNewsByType("cnn", "top"));
+        int result = Integer.valueOf((Integer) jsonNews.get("totalResults"));
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void setNewsFactory_getNewsByType_willReturn20NewsIfTypeIsEverything() throws JSONException { // TODO Balazs nézze meg
+        newsFactory = new NewsFactory();
+        JSONObject jsonNews = new JSONObject(newsFactory.getNewsByType("cnn", ""));
+        int result = Integer.valueOf((Integer) jsonNews.get("totalResults"));
+        System.out.println(jsonNews);
+        assertTrue(result > 10);
     }
 }
