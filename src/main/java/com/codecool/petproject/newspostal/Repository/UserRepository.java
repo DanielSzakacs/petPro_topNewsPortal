d2d019d089fa5b0f49b90d63343ccf0454a078b6
danielszakacs <daniel.sokach@gmail.com>
2019-02-18 11:15:47 +0100

    user repository created. need to test it .

--- src/main/java/com/codecool/petproject/newspostal/Repository/UserRepository.java
+++ src/main/java/com/codecool/petproject/newspostal/Repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.codecool.petproject.newspostal.Repository;
 
+import com.codecool.petproject.newspostal.module.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public interface UserRepository extends JpaRepository<Integer, String>{
+public interface UserRepository extends JpaRepository<User, String>{
 
+    User getByEmail(String email);
 
 }
