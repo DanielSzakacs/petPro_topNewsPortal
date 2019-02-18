a9641d7d4e35ef9bf5d4cbead2944c004858fcb2
danielszakacs <daniel.sokach@gmail.com>
2019-02-18 13:55:38 +0100

    Login class methods created.

--- src/main/java/com/codecool/petproject/newspostal/service/login/LoginHandler.java
+++ src/main/java/com/codecool/petproject/newspostal/service/login/LoginHandler.java
@@ -1,15 +1,40 @@
 package com.codecool.petproject.newspostal.service.login;
 
 import com.codecool.petproject.newspostal.Repository.UserRepository;
+import com.codecool.petproject.newspostal.service.security.BCryptPasswordHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.security.InvalidAlgorithmParameterException;
 
 public class LoginHandler {
 
     @Autowired
     private UserRepository userRepository;
 
-    public boolean isEmailRegistered(String email){
+    @Autowired
+    private BCryptPasswordHandler bCryptPasswordHandler;
+
+    // TODO ez a fő method
+    public boolean isUserEmailAndPasswordCorrect(String email, String password) {
+        if(isEmailRegistered(email)){
+            if(isPasswordCorrect(email, password)){
+                return true;
+            }else{
+                new Exception("Your password is not correct");
+            }
+        }else{
+            new InvalidAlgorithmParameterException("Your email is not correct");
+        }
+        return false;
+    }
+
+    private boolean isEmailRegistered(String email){
         return userRepository.getByEmail(email).email.equals(email);
+    }
+
+    private boolean isPasswordCorrect(String email, String password){
+        String userHashedPassword = userRepository.getByEmail(email).password;
+        return bCryptPasswordHandler.matchPasswords(password, userHashedPassword);
     }
 
     //kell ami visszahesheli a kódot
