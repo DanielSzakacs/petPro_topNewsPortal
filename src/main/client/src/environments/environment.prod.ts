dbeeb45286b27ee04c713163f5599713551f78d0
danielszakacs <daniel.sokach@gmail.com>
2019-02-20 11:12:14 +0100

    front end language text refactored,

--- src/main/client/src/environments/environment.prod.ts
+++ src/main/client/src/environments/environment.prod.ts
@@ -5,10 +5,12 @@ export const environment = {
   rusText: {'World':'Мир', 'Business':'Бизнес',
     'Sport':'Спорт', 'Crypto':'Крипто',
     'In_focus':'Фокус', 'Top_news':'Новости',
-    'Logo': 'ЛОГО', 'Around_the_world': 'Мир', 'All_news': 'Все'},
+    'Logo': 'ЛОГО', 'Around_the_world': 'Мир', 'All_news': 'Все',
+    'Registration' : 'Регистрация', 'Logout' : 'Выйти', 'Login' : 'Войти', 'Email' : 'Ваше email адрес', 'Password' : 'Пароль', 'Submit' : 'Подавать'},
   engText: {'World':'World', 'Business':'Business',
     'Sport':'Sport', 'Crypto':'Crypto',
     'In_focus':'In focus', 'Top_news':'Top stories',
     'Logo': 'LOGO', 'Around_the_world': 'Around the world',
-    'All_news': 'All news'}
+    'All_news': 'All news',
+    'Registration' : 'Registration', 'Logout' : 'Logout', 'Login' : 'Log in', 'Email' : 'Email', 'Password' : 'Password', 'Submit' : 'Submit'}
 };
